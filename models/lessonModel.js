b669f3368a66384bf660f63b81ec9b77d3d2a96c
salahlala <salahlala303@gmail.com>
2025-04-20 06:39:30 +0200

    efactor(lesson) : optimize performance

--- models/lessonModel.js
+++ models/lessonModel.js
@@ -5,12 +5,34 @@ const lessonSchema = new Schema(
     title: {
       type: String,
       required: true,
+      minLength: [3, "Lesson title must be at least 3 characters"],
     },
 
     video: {
-      assetId: { type: String, required: true },
-      playbackId: { type: String, required: true },
-      playbackUrl: { type: String, required: true },
+      assetId: {
+        type: String,
+        required: function () {
+          return this.course && this.course.status === "publish";
+        },
+      },
+      playbackId: {
+        type: String,
+        required: function () {
+          return this.course && this.course.status === "publish";
+        },
+      },
+      playbackUrl: {
+        type: String,
+        required: function () {
+          return this.course && this.course.status === "publish";
+        },
+      },
+      duration: {
+        type: Number, // Duration in seconds
+        required: function () {
+          return this.course && this.course.status === "publish";
+        },
+      },
     },
     chapter: {
       type: Schema.Types.ObjectId,
@@ -25,18 +47,26 @@ const lessonSchema = new Schema(
       type: Number,
       required: true,
     },
-    
+    locked: {
+      type: Boolean,
+      default: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true, toObject: { virtuals: true }, toJSON: { virtuals: true } }
 );
-
 
 // Indexing fields
 lessonSchema.index({ chapter: 1 }); // Index on chapter
-lessonSchema.index({ course: 1 });  // Index on course
-lessonSchema.index({ order: 1 });   // Index on order
+lessonSchema.index({ course: 1 }); // Index on course
+lessonSchema.index({ order: 1 }); // Index on order
 // Optional compound index if needed
 lessonSchema.index({ chapter: 1, order: 1 }); // Compound index on chapter and order
+
+lessonSchema.virtual("formattedDuration").get(function () {
+  const minutes = Math.floor(this.video.duration / 60);
+  const seconds = Math.floor(this.video.duration % 60);
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+});
 
 const Lesson = mongoose.models.Lesson || model("Lesson", lessonSchema);
 export default Lesson;
