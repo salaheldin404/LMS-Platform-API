931a3bf6b01038912eb5d9cbb8f2917af6ac149a
salahlala <salahlala303@gmail.com>
2025-08-31 00:16:18 +0300

    feat(courses): Add new course routes and enhance security

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -12,6 +12,11 @@ import {
   getSearchCourses,
   removeRatingFromCourse,
   getCourseProgressStats,
+  getInstructorCourses,
+  publishCourse,
+  getRatingPercentages,
+  getCourseBySlug,
+  getUserProgressForCourse,
 } from "../controller/courseController.js";
 import { protectRoute } from "../middlewares/protect.js";
 import upload from "../utils/multerConfig.js";
@@ -22,23 +27,31 @@ const router = express.Router();
 // api/v1/courses/courseId/chapters
 
 // router.use('/:courseId/chapters/:chapterId/lessons',lessonRouter)
-router.use(protectRoute);
 
 router.get("/", getAllCourses);
-router.get("/me", getCoursesForCurrentInstructor);
 router.get("/search", getSearchCourses);
-router.get("/:courseId", getCourseById);
-router.post("/", upload.single("image"), createCourse);
+router.get("/user/:userId", getInstructorCourses);
+
+router.get("/me", protectRoute, getCoursesForCurrentInstructor);
+
+router.get("/:courseId([0-9a-fA-F]{24})", protectRoute, getCourseById);
+router.get("/:slug", getCourseBySlug);
+
+router.use(protectRoute);
+router.post("/", createCourse);
 router.patch("/:courseId", upload.single("image"), editCourse);
 router.delete("/:courseId", deleteCourse);
 
+router.post("/:courseId/publish", publishCourse);
+// router.post("/:courseId/rate", addRatingToCourse);
+
+router.get("/:courseId/user-progress", getUserProgressForCourse);
 router.get("/:courseId/stats", getCourseStats);
 router.get("/:courseId/progress-stats", getCourseProgressStats);
-
+// router.get("/:courseId/ratingPercentage", getRatingPercentages);
 router.post("/:courseId/generate-certificate", generateCertifcate);
 
-router.post("/:courseId/rate", addRatingToCourse);
-router.delete("/:courseId/rate", removeRatingFromCourse);
+// router.delete("/:courseId/rate", removeRatingFromCourse);
 
 router.use("/:courseId/chapters", chapterRouter);
 
